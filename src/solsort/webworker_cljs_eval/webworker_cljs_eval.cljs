3860dd7b13d938114d62acaacdccf2b608171c60
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-12-15 09:54:34 +0100

    experimenting with web-workers

--- src/solsort/webworker_cljs_eval/webworker_cljs_eval.cljs
+++ src/solsort/webworker_cljs_eval/webworker_cljs_eval.cljs
@@ -15,4 +15,43 @@
    [clojure.string :as string :refer [replace split blank?]]
    [cljs.core.async :refer [>! <! chan put! take! timeout close! pipe]]))
 
-(render [:div [:h1 "hello world"]])
+(defonce workers (atom {}))
+(defn new-worker []
+  (let [worker (js/Worker. "worker.js")
+        id (random-uuid)]
+    (.postMessage worker (str "ID=" (js/JSON.stringify (str id))))
+    (.postMessage worker "console.log('new worker:', ID)")
+    (swap! workers assoc id worker)
+    (db! [:workers] (conj (db [:workers] []) {:id id}))
+    ))
+
+(new-worker)
+(random-uuid)
+(when-not (db [:code])
+  (db! [:code] "(ns hello)
+     (js/console.log \"here\")
+     (js/console.log \"here2\") "))
+
+(defn compile []
+  (db! [:compiled-code] (db [:code]))
+  )
+
+(defn app []
+  [:div  [input {:db [:code]
+                 :style {:text-align :left
+                         :display :inline-block
+                         :width "50%"
+                         :position :fixed
+                         :height js/window.innerHeight}
+                 :type :textarea}]
+   [:div {:style {:display :inline-block
+                  :position :absolute
+                  :background :red
+                  :width "50%"
+                  :right 0
+                  }}
+    [:button {:on-click compile} "compile"]
+    "compiled-code:"
+    [:pre (db [:compiled-code])]
+    ]])
+(render [app])
